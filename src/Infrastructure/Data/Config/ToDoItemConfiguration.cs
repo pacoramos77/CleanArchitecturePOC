bcf2c610969d5c71d0b778875cadd01aa7a1b30a
Francisco Ramos <pacoramos77@gmail.com>
2023-01-05 13:13:11 +0100

    fix table name

---
+++ src/Infrastructure/Data/Config/ToDoItemConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Core.ToDoListAggregate;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Config;
+
+public class ToDoItemConfiguration : IEntityTypeConfiguration<ToDoItem>
+{
+    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    {
+        builder.ToTable("ToDoItems");
+        builder.Property(t => t.Description).IsRequired();
+    }
+}
--- src/Infrastructure/Data/Config/ToDoListConfiguration.cs
+++ src/Infrastructure/Data/Config/ToDoListConfiguration.cs
@@ -9,6 +9,7 @@ public class ToDoListConfiguration : IEntityTypeConfiguration<ToDoList>
 {
     public void Configure(EntityTypeBuilder<ToDoList> builder)
     {
+        builder.ToTable("ToDoLists");
         builder.Property(t => t.Name).IsRequired();
     }
 }
